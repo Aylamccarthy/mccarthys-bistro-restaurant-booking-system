5c80d10f1fe62af7bcbf8667530580164e1ab41f
Aylamccarthy <aylamarmol@yahoo.co.uk>
2023-06-07 11:59:39 +0000

    Modify models.py

--- bookingsystem/models.py
+++ bookingsystem/models.py
@@ -1,3 +1,23 @@
-from django.db import models
+"""
+Booking System App - Models
+-----------------------
+Models for Booking  System App
+"""
 
-# Create your models here.
+from django.db import models
+from cloudinary.models import CloudinaryField
+from django.conf import settings
+
+
+class Table(models.Model):
+    """Model for the table object"""
+    title = models.CharField(max_length=15, unique=True)
+    free_table_img = CloudinaryField('free_table_img')
+    occupied_table_img = CloudinaryField('occupied_table_img')
+    no_of_guests = models.IntegerField(default=1)
+
+    def __str__(self):
+        return self.code
+
+
+
