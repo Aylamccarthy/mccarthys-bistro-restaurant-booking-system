64cb7608e4025703cc63464b3e0a45660035e49a
Aylamccarthy <aylamarmol@yahoo.co.uk>
2023-06-16 18:45:40 +0000

    Add urls.py in reviews app

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,10 @@
+"""
+Reviews App - Admin
+----------------
+Admin Configuration for Reviews App.
+"""
 from django.contrib import admin
+from reviews.models import Review
 
-# Register your models here.
+
+admin.site.register(Review)
--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,29 @@
-from django.db import models
+"""
+Reviews App - Models
+----------------
+Models for Review App.
+"""
 
-# Create your models here.
+from django.db import models
+import datetime
+from django.conf import settings
+
+
+class Review(models.Model):
+    """Model for Reviews Post"""
+    rate = models.PositiveSmallIntegerField()
+    review_text = models.TextField()
+    now = datetime.datetime.now()
+    date_created_on = models.DateTimeField(
+        default=now.strftime("%Y-%m-%d %H:%M:%S"))
+    date_updated_on = models.DateTimeField(
+        default=now.strftime("%Y-%m-%d %H:%M:%S"))
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
+        to_field='email', blank=True)
+
+    class Meta:
+        ordering = ["date_updated_on"]
+
+    def __str__(self):
+        return f"Review {self.review_text} by {self.author}"
