f2b51ebcb97c96cffe90a82bdabf59c8a4a6ff3a
Aylamccarthy <aylamarmol@yahoo.co.uk>
2023-06-24 21:34:00 +0000

    Modify settings.py and urls.py

--- mccarthysbistro/settings.py
+++ mccarthysbistro/settings.py
@@ -44,6 +44,10 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
@@ -51,8 +55,13 @@ INSTALLED_APPS = [
     # Apps
     'home',
     'reviews',
-    
+
 ]
+
+SITE_ID = 1
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
