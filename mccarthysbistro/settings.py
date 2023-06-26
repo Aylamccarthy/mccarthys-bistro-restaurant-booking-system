d00ac2a6ddeb5aaa66249af2af0b83aacb054796
Aylamccarthy <aylamarmol@yahoo.co.uk>
2023-06-26 19:12:30 +0000

    Install django crispy forms

--- mccarthysbistro/settings.py
+++ mccarthysbistro/settings.py
@@ -95,6 +95,14 @@ TEMPLATES = [
     },
 ]
 
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
 WSGI_APPLICATION = 'mccarthysbistro.wsgi.application'
 
 
@@ -144,6 +152,17 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+# Account Setup
+
+ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_USERNAME_REQUIRED = True
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_VERIFICATION = 'none'
+ACCOUNT_USERNAME_MIN_LENGTH = 4
+LOGIN_URL = '/accounts/login'
+LOGIN_REDIRECT_URL = '/'
 
 
 # Static files (CSS, JavaScript, Images)
