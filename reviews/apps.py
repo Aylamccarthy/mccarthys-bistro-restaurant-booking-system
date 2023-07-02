0788e377c265db0cce26eca3b0c9d69c6249c932
Aylamccarthy <aylamarmol@yahoo.co.uk>
2023-07-02 12:18:22 +0000

    Refactor code to debug

--- reviews/apps.py
+++ reviews/apps.py
@@ -2,5 +2,6 @@ from django.apps import AppConfig
 
 
 class ReviewsConfig(AppConfig):
+    """Review App Configuration"""
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'reviews'
--- reviews/models.py
+++ reviews/models.py
@@ -7,4 +7,3 @@ import datetime
 from django.db import models
 from django.conf import settings
 
-
