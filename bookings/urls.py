7d276065a7933baddc6bfcb9b29fe47380461674
Aylamccarthy <aylamarmol@yahoo.co.uk>
2023-07-04 02:05:43 +0000

    Create urls.py for bookings app

---
+++ bookings/urls.py
@@ -0,0 +1,12 @@
+"""
+Bookings App - URLS
+----------------
+URLS configuration for Bookings App
+
+"""
+from . import views
+from django.urls import path
+
+urlpatterns = [
+
+]
--- mccarthysbistro/settings.py
+++ mccarthysbistro/settings.py
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
     'home',
     'reviews',
     'users',
+    'bookings',
     
     # Others
     'crispy_forms',
