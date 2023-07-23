78b6cc488af82a792edef1d6e2248e399d1304dc
Aylamccarthy <aylamarmol@yahoo.co.uk>
2023-07-23 19:36:23 +0000

    Register menu model to admin

--- menu/admin.py
+++ menu/admin.py
@@ -1,3 +1,19 @@
+"""
+Menu App - Admin
+----------------
+Admin Configuration for Menu App.
+"""
 from django.contrib import admin
 
+from menu.models import Menu
+
+
 # Register your models here.
+@admin.register(Menu)
+class Menu(admin.ModelAdmin):
+    """ Class to display menu items on admin """
+    list_display = (
+        'name',
+        'price',
+        'ingredients',
+    )
