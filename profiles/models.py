0a2f28580b4d0a90496ca7ff01dee65d2bb7e4c7
Aylamccarthy <aylamarmol@yahoo.co.uk>
2023-07-25 21:32:17 +0000

    Update migrations

--- profiles/models.py
+++ profiles/models.py
@@ -1,13 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 """Model for the user profile"""
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(blank=True, max_length=200)
     dietary_requirements = models.CharField(blank=True, max_length=200)
     food_allergies = models.CharField(blank=True, max_length=200)
 
     def __Str__(self):
-        return self.user.username
+        return str(self.user)
+
--- profiles/views.py
+++ profiles/views.py
@@ -1,15 +1,31 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 from .forms import ProfileForm
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 def update_profile(request):
     """View to handle profile update"""
     if request.method == 'POST':
-        form = ProfileForm(request.POST, request.FILES, instance=request.user.profile)
+        form = ProfileForm(request.POST, request.FILES, instance=request.user)
         if form.is_valid():
             form.save()
             return redirect('profile')
     else:
-        form = ProfileForm(instance=request.user.profile)
+        form = ProfileForm(instance=request.user)
 
     return render(request, 'profile.html', {'form': form})
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+
