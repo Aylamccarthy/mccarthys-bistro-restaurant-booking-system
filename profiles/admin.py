02e6cebd78ebd654afe57d6480a07bc28f9f1665
Aylamccarthy <aylamarmol@yahoo.co.uk>
2023-07-25 18:28:33 +0000

    Register Profile model to admin

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Profile
+
 
 # Register your models here.
+@admin.register(Profile)
+class Profile(admin.ModelAdmin):
+    """ Class to display menu items on admin """
+    list_display = (
+        'dietary requirements',
+        'allergies',
+        'favorite meal',
+    )
