5b9f6dc0cdde3b4f083d52844ee9ceb303f8d162
Aylamccarthy <aylamarmol@yahoo.co.uk>
2023-07-25 18:51:40 +0000

    Create view for profile app

--- profiles/admin.py
+++ profiles/admin.py
@@ -3,11 +3,4 @@ from .models import Profile
 
 
 # Register your models here.
-@admin.register(Profile)
-class Profile(admin.ModelAdmin):
-    """ Class to display menu items on admin """
-    list_display = (
-        'dietary requirements',
-        'allergies',
-        'favorite meal',
-    )
+admin.site.register(Profile)
--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ProfileForm
 
-# Create your views here.
+
+def update_profile(request):
+    """View to handle profile update"""
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES, instance=request.user.profile)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
+    else:
+        form = ProfileForm(instance=request.user.profile)
+
+    return render(request, 'profile.html', {'form': form})
