42151d55e88f71c76e6c1834f6c1626acd88f5e2
Aylamccarthy <aylamarmol@yahoo.co.uk>
2023-07-31 18:51:16 +0000

    Add reviews app urls

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,5 +1,12 @@
-from . import views
+"""
+Reviews App - URLS
+----------------
+URLS configuration for Review App.
+"""
 from django.urls import path
+from . import views
 
 
-
+urlpatterns = [
+    path('reviews', views.Review.as_view(), name='reviews'),
+]
