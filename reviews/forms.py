be8cf5a169195a1dd9fad95cd33dd2a6b1dc9c82
Aylamccarthy <aylamarmol@yahoo.co.uk>
2023-07-31 18:36:13 +0000

    Create a form to handle review submissions

---
+++ reviews/forms.py
@@ -0,0 +1,24 @@
+"""
+Review App - Forms
+----------------
+Forms for Review App.
+"""
+from django import forms
+from review.models import Review
+
+
+class ReviewForm(forms.ModelForm):
+    """Form for add/update review"""
+    rate = forms.IntegerField(widget=forms.NumberInput(
+        attrs={'id': 'rateValue', 'type': 'hidden'}))
+    review_text = forms.CharField(widget=forms.Textarea(attrs={
+        'id': 'addReviewText', 'class': 'form-control',
+        'rows': '6', 'cols': '100'}), label="Add a message:")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['rate'].required = False
+
+    class Meta:
+        model = Review
+        fields = ['review_text', 'rate']
