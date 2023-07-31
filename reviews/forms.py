9af0b09ac4d247397f5f93f64ee889d5794ab616
Aylamccarthy <aylamarmol@yahoo.co.uk>
2023-07-31 19:36:02 +0000

    Create form to update review

--- reviews/forms.py
+++ reviews/forms.py
@@ -22,3 +22,20 @@ class ReviewForm(forms.ModelForm):
     class Meta:
         model = Review
         fields = ['review_text', 'rate']
+
+
+class UpdateReviewForm(forms.ModelForm):
+    """Form for add/update review"""
+    rate = forms.IntegerField(widget=forms.NumberInput(
+        attrs={'id': 'updateRateValue', 'type': 'hidden'}))
+    review_text = forms.CharField(widget=forms.Textarea(attrs={
+        'id': 'updateReviewText', 'class': 'form-control',
+        'rows': '6', 'cols': '100'}), label="Update your message")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['rate'].required = False
+
+    class Meta:
+        model = Review
+        fields = ['review_text', 'rate']
