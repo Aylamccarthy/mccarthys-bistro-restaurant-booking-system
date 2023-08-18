4c81830a62e8985d4012764756170193b1a7b87a
Aylamccarthy <aylamarmol@yahoo.co.uk>
2023-08-18 22:32:21 +0000

    Add list display and filter on reviews app

--- reviews/admin.py
+++ reviews/admin.py
@@ -7,4 +7,13 @@ from django.contrib import admin
 from reviews.models import Review
 
 
-admin.site.register(Review)
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = (
+        "author",
+        "review_text",
+        "date_created_on",
+    )
+    list_filter = (
+        "date_created_on",
+    )
